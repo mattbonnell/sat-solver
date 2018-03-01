ad799843e86a8a023545561e9feedf9dccb06bc9
mdcbonne <mdcbonne@bf7adb0c-0a71-4f8a-9a9b-06af373ef5e1>
2018-03-01 15:57:08 +0000

    Implement main.c
    git-svn-id: https://ecesvn.uwaterloo.ca/courses/ece108/w18/students/mdcbonne/SatSolver@2305 bf7adb0c-0a71-4f8a-9a9b-06af373ef5e1

--- src/main/main.c
+++ src/main/main.c
@@ -6,11 +6,29 @@
 
 #include <stdio.h>
 #include "dimacs_reader.h"
+#include <math.h>
+
+struct BooleanExpression {
+	int numvars;
+	int numclauses;
+	int ** clauses;
+}
+BooleanExpression * currentExpression;
+
+int currentClauseIndex;
 
 void init(int numvars, int numclauses) {
 	// Callback function that gets passed to read_dimacs().
 	// Indicates start of a new expression with up to numvars variables
 	// and numclauses clauses.
+	if(currentExpression != 0){
+		free(currentExpression);
+	}
+	currentExpression = malloc(sizeof(currentExpression));
+	currentExpression->numvars = numvars;
+	currentExpression->numclauses = numclauses;
+	currentExpression->clauses = malloc(numclauses * sizeof(*(currentExpression->clauses)));
+	currentClauseIndex = 0;
 }
 
 void add_clause(int numlits, int literals[]) {
@@ -18,12 +36,53 @@ void add_clause(int numlits, int literals[]) {
 	// The literals argument is an array of ints, numlits is its length.
 	// Each literal is the index (starting from 1) of a variable.
 	// Negative integers represent logical negations of variables.
+	currentExpression->clauses[currentClauseIndex] = malloc((numlits + 1) * sizeof(*(currentExpression->clauses[currentClauseIndex])));
+	currentExpression->clauses[currentClauseIndex][0] = numlits;
+	for(int i = 1; i < numlits + 1; i++){
+		currentExpression->clauses[currentClauseIndex][i] = literals[i];
+	}
+	currentClauseIndex++;
 }
 
 char *classify() {
 	// Evaluate the current expression and return one of
 	// "satisfiable", "unsatisfiable" or "tautology".
-	return "unsatisfiable";
+	int numVariables = currentExpression->numvars;
+	int numClauses = currentExpression->numclauses;
+	int * truthValues = malloc(numVariables * sizeof(*truthValues));
+	int numPermutations = (int)pow((double)2, (double)numVariables);
+	int truePermutations = 0;
+	for(int i = 0; i < numPermutations; i++){
+		for(int j = 0; j < numVariables; j++) {
+			truthValues[j] = i >> j & 1;
+		}
+		int expressionIsTrue = true;
+		for(int k = 0; k < numClauses; k++){
+			int * currentClause = currentExpression->clauses[0];
+			int numlits = currentClause[0];
+			bool clauseIsTrue = false
+			for (int l = 1; l < numlits + 1; l++){
+				if(truthValues[currentClause[l] - 1]) == 1) {
+					clauseIsTrue = true;
+					break;
+				}
+			}
+			if (clauseIsTrue == false){
+				expressionIsTrue = false;
+				break;
+			}
+		}
+		if (expressionIsTrue){
+			truePermutations++;
+		}
+	}
+	if (truePermutations == 0){
+		return "unsatisfiable";
+	} else if (truePermutations == numPermutations){
+		return "tautology";
+	} else {
+		return "satisfiable";
+	}
 }
 
 int main(int argc, char *argv[]) {
